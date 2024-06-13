51f14f0b2099c103781861d42799c13ad09fc56d
M. Khaidar <haidar038@gmail.com>
2024-06-14 01:09:19 +0900

    Changes to be committed: 	new file:   App/static/icon512_maskable.png 	new file:   App/static/icon512_rounded.png 	new file:   App/static/manifest.json 	new file:   App/static/sw.js 	modified:   App/templates/base.html 	modified:   App/views/routes.py

--- App/views/routes.py
+++ App/views/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, current_app, request, render_template, flash, redirect, url_for, jsonify, session
+from flask import Blueprint, current_app, request, render_template, flash, redirect, url_for, jsonify, session, send_file
 from flask_login import login_required, current_user
 from flask_sqlalchemy import pagination
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -10,6 +10,15 @@ import json, requests, secrets, os
 # from models import User, DataPangan, Kelurahan
 
 views = Blueprint('views', __name__)
+
+# MANIFEST
+@views.route('/manifest.json')
+def serve_manifest():
+    return send_file('manifest.json', mimetype='application/manifest+json')
+
+@views.route('/sw.js')
+def serve_sw():
+    return send_file('sw.js', mimetype='application/javascript')
 
 @views.route('/')
 def index():
